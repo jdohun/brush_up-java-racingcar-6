9fc883b3c964a824a987679b9e9e28015c6d1b0b
jdohun <wjdehgns97@naver.com>
2023-11-24 00:04:04 +0900

    feat: CarName 정적 팩토리 메서드 제거
    CarName.java

--- src/main/java/racingcar/domain/model/CarName.java
+++ src/main/java/racingcar/domain/model/CarName.java
@@ -5,16 +5,12 @@ public class CarName {
     private static final int LIMIT_LENGTH = 5;
     private String name;
 
-    private CarName(String name) {
+    public CarName(String name) {
+        validateLength(name);
         this.name = name;
     }
 
-    public static CarName from(String name) {
-        validateLength(name);
-        return new CarName(name);
-    }
-
-    private static void validateLength(String name) {
+    private void validateLength(String name) {
         if (LIMIT_LENGTH < name.length()) {
             throw new IllegalArgumentException(ERROR_LENGTH);
         }
