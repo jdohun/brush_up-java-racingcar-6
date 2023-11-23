1aff78eb98f568eef90d0151259c2a65b46501d5
DH Jeong <wjdehgns97@gmail.com>
2023-11-23 20:35:45 +0900

    feat: 자동차 이름을 입력 받아서 LineUp 객체를 생성하는 정적 팩토리 메서드 구현
    LineUp.java

--- src/main/java/racingcar/domain/model/LineUp.java
+++ src/main/java/racingcar/domain/model/LineUp.java
@@ -1,5 +1,6 @@
 package racingcar.domain.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LineUp {
@@ -7,5 +8,13 @@ public class LineUp {
 
     public LineUp(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public static LineUp from(List<String> lineUpSource) {
+        List<Car> cars = new ArrayList<>();
+        for (String inputName : lineUpSource) {
+            cars.add(new Car(inputName));
+        }
+        return new LineUp(cars);
     }
 }
