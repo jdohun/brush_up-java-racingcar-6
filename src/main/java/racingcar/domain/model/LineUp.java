4fef66a49e61025481c7d73fcb7f1e9c76760235
jdohun <wjdehgns97@naver.com>
2023-11-23 18:52:00 +0900

    feat: 대회에 참여하는 자동차들를 저장하는 객체 추가
    LineUp.java

---
+++ src/main/java/racingcar/domain/model/LineUp.java
@@ -0,0 +1,11 @@
+package racingcar.domain.model;
+
+import java.util.List;
+
+public class LineUp {
+    private final List<Car> cars;
+
+    public LineUp(List<Car> cars) {
+        this.cars = cars;
+    }
+}
