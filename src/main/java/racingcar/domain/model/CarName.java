b0fc639ea25ec2dd17fd1eda3f6be984362b75ff
DH Jeong <wjdehgns97@gmail.com>
2023-11-24 01:28:45 +0900

    feat: CarName 생성 시 null 과 빈 문자열 검증 후 예외처리하는 기능 추가
    CarName.java

--- src/main/java/racingcar/domain/model/CarName.java
+++ src/main/java/racingcar/domain/model/CarName.java
@@ -1,13 +1,27 @@
 package racingcar.domain.model;
 
+import java.util.Objects;
+
 public class CarName {
     private static final String ERROR_LENGTH = "이름은 5자 이하만 가능합니다.";
     private static final int LIMIT_LENGTH = 5;
     private String name;
 
     public CarName(String name) {
-        validateLength(name);
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        Objects.requireNonNull(name);
+        validateEmpty(name);
+        validateLength(name);
+    }
+
+    private void validateEmpty(String name){
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("빈 문자열은 이름이 될 수 없습니다.");
+        }
     }
 
     private void validateLength(String name) {
