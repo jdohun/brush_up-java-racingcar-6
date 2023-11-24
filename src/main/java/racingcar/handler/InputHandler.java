477df0c4c8b9e91fa4a06cfd3434c04eb1a994aa
DH Jeong <wjdehgns97@gmail.com>
2023-11-24 19:35:50 +0900

    feat: 입력한 값이 null 일 때, 빈 값만 가질 때 예외 처리 하는 기능 구현
    InputHandler.java

--- src/main/java/racingcar/handler/InputHandler.java
+++ src/main/java/racingcar/handler/InputHandler.java
@@ -2,14 +2,14 @@ package racingcar.handler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputHandler {
+    private static final Pattern FORMAT_INVALID_INPUT_EMPTY = Pattern.compile("^\\s*$");
     private static final Pattern FORMAT_INVALID_INPUT_NAMES = Pattern.compile("^\\s*,|,\\s*,|,\\s*$");
     private static final Pattern FORMAT_VALID_INPUT_NUMBER_OF_ATTEMPTS = Pattern.compile("^\\d+$");
-    private static final String ERROR_INVALID_INPUT_NAMES = "자동차 이름 입력 형식에 맞지 않습니다.";
-    private static final String ERROR_INVALID_INPUT_NUMBER_OF_ATTEMPTS = "시도 횟수 입력 형식에 맞지 않습니다.";
     private static final String DELIMITER_INPUT_NAMES = ",";
 
     public static InputHandler getInstance() {
@@ -27,21 +27,46 @@ public class InputHandler {
     }
 
     private void validateInputNames(String inputNames) {
+        validateNotNull(inputNames);
+        validateNotEmpty(inputNames);
+        validateInputNamesFormat(inputNames);
+    }
+
+    private void validateInputNamesFormat(String inputNames) {
         Matcher matcher = FORMAT_INVALID_INPUT_NAMES.matcher(inputNames);
         if (matcher.find()) {
-            throw new IllegalArgumentException(ERROR_INVALID_INPUT_NAMES);
+            throw new IllegalArgumentException("자동차 이름 입력 형식에 맞지 않습니다.");
         }
     }
 
     public int parseInputNumberOfAttempts(String inputNumberOfAttempts) {
+        validateNotNull(inputNumberOfAttempts);
+        validateNotEmpty(inputNumberOfAttempts.trim());
         validateInputNumberOfAttempts(inputNumberOfAttempts);
         return Integer.parseInt(inputNumberOfAttempts);
     }
 
     private void validateInputNumberOfAttempts(String inputNumberOfAttempts) {
+        validateInputNumberOfAttemptsFormat(inputNumberOfAttempts);
+    }
+
+    private void validateInputNumberOfAttemptsFormat(String inputNumberOfAttempts) {
         Matcher matcher = FORMAT_VALID_INPUT_NUMBER_OF_ATTEMPTS.matcher(inputNumberOfAttempts);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException(ERROR_INVALID_INPUT_NUMBER_OF_ATTEMPTS);
+            throw new IllegalArgumentException("시도 횟수 입력 형식에 맞지 않습니다.");
+        }
+    }
+
+    private void validateNotNull(String input) {
+        if (null == input) {
+            throw new IllegalArgumentException("null 을 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateNotEmpty(String input) {
+        Matcher matcher = FORMAT_INVALID_INPUT_EMPTY.matcher(input);
+        if (matcher.matches()) {
+            throw new IllegalArgumentException("빈 값을 입력할 수 없습니다.");
         }
     }
 
