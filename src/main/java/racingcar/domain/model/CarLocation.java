e3c10426c60c6fd6ec25d36838fdc89563bdfdb1
jdohun <wjdehgns97@naver.com>
2023-11-24 18:33:40 +0900

    feat: 이동 위치 equals, hashCode 오버라이드 및 비교 후 큰 값 반환하는 기능 구현
    CarLocation.java

--- src/main/java/racingcar/domain/model/CarLocation.java
+++ src/main/java/racingcar/domain/model/CarLocation.java
@@ -24,4 +24,29 @@ class CarLocation {
         }
         return sb.toString();
     }
+
+    public CarLocation compareTo(CarLocation targetLocation) {
+        if (this.location >= targetLocation.location) {
+            return this;
+        }
+        return targetLocation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (null == obj || !(obj instanceof CarLocation target)) {
+            return false;
+        }
+
+        return this.location == target.location;
+    }
+
+    @Override
+    public int hashCode() {
+        return location;
+    }
 }
