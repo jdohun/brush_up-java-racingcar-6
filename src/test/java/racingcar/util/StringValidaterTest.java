d881743c995736b65b5c39a3c13035cbec1a121f
DH Jeong <wjdehgns97@gmail.com>
2023-11-27 17:23:51 +0900

    test: 문자열 앞뒤는 공백을 허용하지 않으면서 문자열 사이는 공백을 허용하는 패턴을 이용한 검증 기능 테스트
    StringValidaterTest.java

--- src/test/java/racingcar/util/StringValidaterTest.java
+++ src/test/java/racingcar/util/StringValidaterTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringValidaterTest {
@@ -29,10 +30,17 @@ class StringValidaterTest {
 
     @DisplayName("공백으로 시작하거나 공백으로 끝나는 문자열을 입력 받으면 예외처리가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {" test", "test ", " test "})
-    void validateHasNotWhiteSpace(String startOrFinishByWhiteSpace) {
-        assertThatThrownBy(() -> StringValidater.validateHasNotWhiteSpace(startOrFinishByWhiteSpace))
+    @ValueSource(strings = {" test", "  test", " test ", "  test  ", "test ", "test  " })
+    void validateHasNotSurroundingWhiteSpace(String surroundingWhiteSpace) {
+        assertThatThrownBy(() -> StringValidater.validateHasNotSurroundingWhiteSpace(surroundingWhiteSpace))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("문자열의 시작과 끝에 공백을 입력할 수 없습니다.");
+    }
+
+    @DisplayName("문자열 사이의 공백은 예외처리가 발생하지 않는다.")
+    @Test
+    void validateHasWhiteSpace() {
+        assertThatCode(() -> StringValidater.validateHasNotSurroundingWhiteSpace("te st"))
+                .doesNotThrowAnyException();
     }
 }
