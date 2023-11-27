13a505637aad178c0798bc870e877edabb8ac8b2
DH Jeong <wjdehgns97@gmail.com>
2023-11-27 16:57:21 +0900

    feat: 문자열의 시작과 끝에 공백을 입력하면 예외를 발생시키는 기능 추가
    StringValidater.java

--- src/main/java/racingcar/util/StringValidater.java
+++ src/main/java/racingcar/util/StringValidater.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class StringValidater {
 
     public static final Pattern FORMAT_INVALID_STRING_EMPTY = Pattern.compile("^\\s*$");
-
+    public static final Pattern FORMAT_INVALID_STRING_HAS_WHITE_SPACE = Pattern.compile("^[\\s]*[\\w]*||[\\s]*[\\w]*[\\s]*||[\\w]*[\\s]*$");
 
     private StringValidater() {
     }
@@ -21,6 +21,13 @@ public class StringValidater {
         Matcher matcher = FORMAT_INVALID_STRING_EMPTY.matcher(input);
         if (matcher.matches()) {
             throw new IllegalArgumentException("빈 문자열을 입력할 수 없습니다.");
+        }
+    }
+
+    public static void validateHasNotWhiteSpace(String input) {
+        Matcher matcher = FORMAT_INVALID_STRING_HAS_WHITE_SPACE.matcher(input);
+        if (matcher.matches()) {
+            throw new IllegalArgumentException("문자열의 시작과 끝에 공백을 입력할 수 없습니다.");
         }
     }
 }
