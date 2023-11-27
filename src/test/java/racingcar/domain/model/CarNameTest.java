fd416c5ef31f24b997cebced86884a6a91d11600
DH Jeong <wjdehgns97@gmail.com>
2023-11-27 17:33:17 +0900

    refactor: 사용하지 않는 import 제거
    CarNameTest.java

--- src/test/java/racingcar/domain/model/CarNameTest.java
+++ src/test/java/racingcar/domain/model/CarNameTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.util.StringValidater;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
